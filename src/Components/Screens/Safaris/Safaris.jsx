5ec6014b90f0e8480f4287c29abd7b245cec3ba7
Je4stine <je4stine@gmail.com>
2023-01-24 16:28:01 +0300

    Homepage loader spinner

--- src/Components/Screens/Safaris/Safaris.jsx
+++ src/Components/Screens/Safaris/Safaris.jsx
@@ -8,7 +8,12 @@ import Navreveal from '../../Navbar/Navreveal';
 
 import Footer from '../../Footer/Footer';
 
-import Destinations from '../../Destination/Destination'
+import Destinations from '../../Destination/Destination';
+
+import ChatBot from '../../ChatBot/ChatBot';
+
+import {AiOutlineArrowDown} from 'react-icons/ai';
+
 
 const Safaris =()=>{
     const [colorChanged, setColorChanged]= useState(false);
@@ -28,13 +33,20 @@ const Safaris =()=>{
                 colorChanged ? <Navreveal/> : <Navbar/>
             }
            <div className=' parallax1'>
-                <div className=" absolute top-[40%] left-[40%] max-[768px]:left-[32%]">
+                <div className=" absolute top-[40%] left-[45%] max-[768px]:left-[32%]">
                     <h1 className=" text-white text-6xl font-bold max-[768px]:text-4xl ">SAFARIS</h1>
                 </div>
+                <div className=" absolute top-[90%] left-[50%] max-[768px]:left-[40%] animate-bounce">
+                      <a href="#destinations">  <AiOutlineArrowDown size={60} color="white"/></a>
+                </div>
            </div>
-           <div>
+           <div id="destinations">
                 <Destinations/>
            </div>
+           <ChatBot/>
+           <div>
+                <img src={require('../../../Assets/kanairo-black.svg').default } alt="Kanairo"/>
+            </div>
            <Footer/>
         </div>
     )
