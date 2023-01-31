77f093f16445e2ed7513ff76aa2d0f2ca964bedc
Je4stine <je4stine@gmail.com>
2023-01-31 17:15:32 +0300

    Struggles tu

--- src/Components/Screens/SafariDetails/MaraSerengeti.jsx
+++ src/Components/Screens/SafariDetails/MaraSerengeti.jsx
@@ -8,20 +8,63 @@ import Footer from "../../Footer/Footer";
 
 import {MdKeyboardArrowDown} from 'react-icons/md';
 
+import Faq from "react-faq-component";
+
 
 const Mara =()=>{
-    const [day1, setDay1]= useState(false);
+    const data = {
+        title: "Iteneraries",
+        rows: [
+            {
+                title: <strong> <h1>Day 1: Nairobi</h1></strong>,
+                content: `Arrival and transfer to one of Kenya’s most luxurious hotels in the city; The Norfolk hotel Nairobi
+                Included:
+                Full board accommodation; Bed, dinner breakfast and lunch. <img src={Mara1} />`,
+            },
+            {
+                title: "Nunc maximus, magna at ultricies elementum",
+                content:
+                    "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
+            },
+            {
+                title: "Curabitur laoreet, mauris vel blandit fringilla",
+                content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
+                Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
+                Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
+                Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
+            },
+            {
+                title: "What is the package version",
+                content: <p>current version is 1.2.1</p>,
+            },
+        ],
+    };
 
-    function toggle (){
-        setDay1(!day1)
-    }
+
+    const styles = {
+        // bgColor: 'white',
+        titleTextColor: "black",
+        rowTitleColor: "black",
+        // rowContentColor: 'grey',
+        // arrowColor: "red",
+    };
+    
+    const config = {
+        // animate: true,
+        // arrowIcon: "V",
+        // tabFocus: true
+    };
+
+
+
+
     return (
         <div className=" bg-white flex flex-col flex-1 items-center justify-center">
             <Navbar/>
             <div className=" flex flex-col items-center">
                 <p>Mara 1</p>
                 <div>
-                    <img src={Mara1} alt=" Mara Camping" className=" m-auto rounded-md"/>
+                    <img src={Mara1} alt=" Mara Camping" className=" m-auto rounded-lg p-5 md:px-[400px]"/>
                     <div className=" md:px-[400px] p-10">
                         <h1>Itenerary Highlight</h1>
                         <p className=" font-[SourceSerifPro-Regular]">
@@ -42,19 +85,16 @@ const Mara =()=>{
                         </p>
                     </div>
                 </div>
-                <h1>Itenerary</h1>
                 <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
-                <div className=" flex justify-center items-center">
-                    <strong><h1 className=" mr-10">DAY 1: NAIROBI KENYA</h1></strong>
-                    <span><MdKeyboardArrowDown size={30}/></span>
-                    {/* <div>
-                        <img/>
-                        <p>
-                        Arrival and transfer to one of Kenya’s most luxurious hotels in the city; The Norfolk hotel Nairobi
-                        Included:
-                        Full board accommodation; Bed, dinner breakfast and lunch.
-                        </p>
-                    </div> */}
+                <div className=" flex justify-center items-center flex-col ">
+                    
+                    <div className=" p-5">
+                        <Faq
+                            data={data}
+                            styles={styles}
+                            config={config}
+                        />
+                    </div>
                 </div>
             </div> 
                 <img src={require('../../../Assets/kanairo-black.svg').default } alt="Kanairo"/>
