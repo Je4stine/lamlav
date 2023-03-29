023d222c547c8ac0ccdbbe30d32055e0a0fceb99
Je4stine <je4stine@gmail.com>
2023-03-29 13:53:05 +0300

    Build38

--- src/Components/Screens/About/About.jsx
+++ src/Components/Screens/About/About.jsx
@@ -15,6 +15,7 @@ import {AiOutlineArrowDown} from 'react-icons/ai';
 import Luxury from '../../../Assets/luxury.jpg';
 
 import About2 from '../../../Assets/about2.jpg';
+import Callfor from "../../Contact/Callfor";
 
 
 
@@ -43,38 +44,62 @@ const AboutUs =()=>{
                       <a href="#about">  <AiOutlineArrowDown size={60} color="white"/></a>
                 </div>
            </div>
-           <div className="md:flex bg-white mt-[50px] mx-[50px]">
-                    <div className=" flex flex-col flex-1 mr-5">
-                        <img src={Luxury} alt='Luxury' className="mb-5 w-[100%]"/>
-                        <img src={Luxury} alt='Luxury' className="mb-5"/>
-                    </div>
-                    <div className=" flex flex-col flex-1">
-                        <img src={About2} alt='About' className="mb-5 w-[93%]"/>
-                        <img src={About2} alt='About' className="mb-5 w-[93%]"/>
-                    </div>
-               </div>
-           <div className=" lg:px-[550px] p-10 py-10 bg-white flex flex-col" id="about">
+
+           <div className=" lg:px-[200px] p-10 py-10 bg-white flex flex-col" id="about">
                
                 <h1 className=" text-4xl font-[SourceSerifPro-Black] mb-3">What really  sets us apart from others? </h1>
-                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md">
-                     It's  our ironclad commitment  to providing  the best possible experiences for our clients.
-                    You will always be treated  like a family  from the moment you book your Safari with us. Our team of knowledgeable  and professional  guides will go above and beyond to make sure your trip is safe, comfortable and unabashedly indulgent.
-                    We pride  ourselves  on  our  attention to details, and take great care in selecting  only the finest accommodation and transport  options for our tours.
-                    Our intimate  group  sizes allows  us to provide  a more personalised  experience, and our focus on sustainability  means  that we operate  in a way that is both responsible  and respectful  of the local communities  and the environment. 
+                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md mt-5 mx-[50px]">
+                     1. It's  our iron clad commitment  to providing  the best possible experiences for our clients. 
                 </p>
-                <h1 className=" text-4xl font-[SourceSerifPro-Black] mb-3">
-                    So, why choose  Lamlav  Safaris  for your  next adventure?
-                </h1>
-                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md">
-                    Simply put, we offer the highest  level of service,  the most immersive and authentic  experiences  and the best value  for your  money. We can't wait  to show you the beauty  of East Africa and create lasting  memories  with you!
-                </p>
-                <h1 className=" text-4xl font-[SourceSerifPro-Black] mb-3">
+
+                <div className="lg:flex flex flex-col justify-center items-center bg-white mt-[50px] mx-[50px]">
+                    <div className=" lg:flex mr-5 ">
+                        <img src={Luxury} alt='Luxury' className="mb-5 lg:w-1/2 mr-5 w-full"/>
+                        <img src={Luxury} alt='Luxury' className="mb-5 lg:w-1/2"/>
+                    </div>
+               </div>
+                <p className="mb-10 font-[SourceSerifPro-Regular] md:text-xl text-md mx-[50px]">  You will always be treated  like a family  from the moment you book your Safari with us.</p>
+
+                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md mx-[50px]">  2. Our team of knowledgeable  and professional  guides will go above and beyond to make sure your trip is safe, comfortable and unabashedly indulgent.</p>
+
+                <div className="lg:flex flex flex-col justify-center items-center bg-white mx-[50px]">
+                    <div className=" lg:flex mr-5 ">
+                        <img src={Luxury} alt='Luxury' className="mb-5 lg:w-1/2 mr-5 w-full"/>
+                        <img src={Luxury} alt='Luxury' className="mb-5 lg:w-1/2"/>
+                    </div>
+               </div>
+             <p className="mb-10 font-[SourceSerifPro-Regular] md:text-xl text-md mx-[50px]"> We pride  ourselves  on  our  attention to details, and take great care in selecting  only the finest accommodation and transport  options for our tours.</p>
+                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md mx-[50px]">
+                    3.Our intimate  group  sizes allows  us to provide  a more personalised  experience, and our focus on sustainability  means  that we operate  in a way that is both responsible  and respectful  of the local communities  and the environment.                </p>
+                <div className="lg:flex flex flex-col justify-center items-center bg-white mt-[50px] mx-[50px]">
+                    <div className=" lg:flex mr-5 ">
+                        <img src={Luxury} alt='Luxury' className="mb-5 lg:w-1/2 mr-5 w-full"/>
+                        <img src={Luxury} alt='Luxury' className="mb-5 lg:w-1/2"/>
+                    </div>
+               </div>
+
+                <h1 className=" text-4xl font-[SourceSerifPro-Black] mb-3 mx-[50px]">
                     Our trips
                 </h1>
-                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md">
-                    We have developed an unsurpassed ability to deliver meticulously planned tailor-made trips around the tastes and interests of every client, with a record breaking satisfaction.  Our team of specialist staffs work corporately to design these trips for our guests to their finest details around their tastes, interests, preference and budget - without compromising on the quality. These specialists are professionals who go extra miles to give you the finest, most unique and unforgettable safari experience in East Africa. You are indeed in safe hands when you plan your trip with us because we listen keenly to your desires and carefully design a stylish and original trip that perfectly matches your desires.
+                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md mx-[50px]">
+                    Lamlav's ability to deliver meticulously planned tailor-made trips around the tastes and interests of every client, with a record breaking satisfaction is unsurpassed.
                 </p>
+                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md mx-[50px]">
+                    Our team of specialist staff work corporate to design three trips for our guests to their finest details around their tastes, ineterest, preference and budget; withought compromising on quality
+                </p>
+                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md mx-[50px]">
+                    These specialists are professional who go extra miles to give you the finest, most unique and unforgetable safari experience in East Africa. You are indeed in safe hands when you plan your trip with us 
+                    because we listen keenly to you desires and carefully design a stylish and original trip that perfectly matches your desires... 
+                </p>
+                <p className="mb-6 font-[SourceSerifPro-Regular] md:text-xl text-md mx-[50px]">
+                    We will continously improve our skills and expertise because we are dedicated to providing excellent service, ensuring high standards and fullfilling our social obligations. 
+                    This devotion to hospitality, quality and social responsibility is unwavering, and is the reasons why we will never stop refining our craft
+                </p>
+                <h1 className=" text-4xl font-[SourceSerifPro-Black] mb-3 mx-[50px]">
+                    Our Partners
+                </h1>
            </div>
+           <Callfor/>
            <ChatBot/>
            <div>
                 <img src={require('../../../Assets/kanairo-black.svg').default } alt="Kanairo"/>
