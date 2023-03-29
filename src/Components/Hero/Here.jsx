338eb5f6f57c55f28e1a549e2087dd6405c154cd
Je4stine <je4stine@gmail.com>
2023-03-29 11:17:27 +0300

    Build34

--- src/Components/Hero/Here.jsx
+++ src/Components/Hero/Here.jsx
@@ -5,7 +5,7 @@ const Hero =()=>{
        <div className='parallax'>
             <div className=' w-full h-full'>
                 <h1 className=' text-[#fff] absolute font-[SourceSerifPro-Regular] lg:top-[32vh] top-[32vh] lg:text-7xl left-10 md:left-16 mt-5 md:text-5xl text-3xl lg:left-10 ' >Explore Africa in Style</h1>
-                <h1 className=' text-white absolute font-[SourceSerifPro-Regular] lg:top-[42vh] top-[38vh] lg:text-7xl left-10 md:left-16 mt-5 md:text-5xl md:top-[39vh] text-3xl lg:left-10  ' >With Africas Best</h1>
+                <h1 className=' text-white absolute font-[SourceSerifPro-Regular] lg:top-[42vh] top-[38vh] lg:text-7xl left-10 md:left-16 mt-5 md:text-5xl md:top-[39vh] text-3xl lg:left-10  ' >With Africa's Best</h1>
                 <h1 className=' text-white absolute font-[SourceSerifPro-Regular] lg:top-[52vh] top-[45vh] lg:text-7xl left-10 md:left-16 mt-5 md:text-5xl text-3xl lg:left-10 ' >Safari Outfitter</h1>
                 <div className=' absolute top-[67vh] text-white border border-white rounded-md p-3 hover:bg-white hover:text-black duration-500 left-16'><a href='#Destination'>Learn More</a></div>
             </div> 
--- src/Components/Why/Why.jsx
+++ src/Components/Why/Why.jsx
@@ -2,18 +2,21 @@ import Adventure from '../../Assets/cranes.jpg';
 
 const Why =()=>{
     return (
-        <div className=' lg:flex lg:flex-row bg-white rounded-md lg:p-2 p-1 flex flex-col m-auto items-center justify-center' id='why'>
-            <div className=' flex flex-col max-[768px]:items-center'>
-                <img src={Adventure} alt="Adventure" className=" rounded-md my-5 mx-10"/>                              
-            </div>
-            <div className='p-5 w-3/4 max-[768px]:p-1 flex flex-col justify-center'>
-                <h1 className=' mb-5 text-2xl text-[#f15d30] max-[768px]:text-xl mt-5'> Welcome to Lamlav Leisure Safaris</h1>
-                <h1 className=' font-bold text-5xl mb-10 max-[768px]:text-xl'>
+        <div className=' bg-white rounded-md lg:p-2 p-1 flex flex-col m-auto items-center justify-center' id='why'>
+          
+            <div className='p-5 flex flex-col justify-center items-center'>
+                <h1 className=' font-bold text-4xl lg:text-5xl mb-10 max-[768px]:text-xl'>
                         It's time to start your adventure
                 </h1>
-                <p className=' font-[SourceSerifPro-Regular] lg:text-lg text-md mb-5'>
-                    We are globally renowned for operating some of the most luxurious and distinct safaris in Kenya, Tanzania, Uganda and Rwanda, and for  taking our esteemed guests to some of  most elegant and experimental lodges and camps - where each lodge or camp impeccably captures the  true essence of the wild and its natural surroundings.                
-                </p>
+                <div className=' flex flex-col items-center'>
+                    <img src={Adventure} alt="Adventure" className=" rounded-md mx-10"/>                              
+                </div>
+                <div className=' p-5'>
+                    <p className=' font-[SourceSerifPro-Regular] lg:text-lg text-md mb-5'>
+                        We are globally renowned for operating some of the most luxurious and distinct safaris in Kenya, Tanzania, Uganda and Rwanda, and for  taking our esteemed guests to some of  most elegant and experimental lodges and camps - where each lodge or camp impeccably captures the  true essence of the wild and its natural surroundings.                
+                    </p>
+                </div>
+              
                 <a href='/about'><div className=' border-black border-[1px] rounded-md hover:bg-[#f15d30] hover:text-white h-[50px] w-[130px] flex items-center justify-center'>
                     <span >Discover more</span>
                 </div></a>
