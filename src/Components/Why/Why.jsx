a63c16bf36eb9bc33f8f4fef9b73a032676104a1
Je4stine <je4stine@gmail.com>
2023-04-04 17:13:01 +0300

    Build 10000

--- src/Components/Why/Why.jsx
+++ src/Components/Why/Why.jsx
@@ -13,7 +13,7 @@ const Why =()=>{
                 </div>
                 <div className=' p-5'>
                     <p className=' font-[SourceSerifPro-Regular] lg:text-lg text-md mb-5'>
-                        Lamlav Leisure Safaris is globally renowned for operationg some of the most luxurious and distant Safaris in Kenya, Tanzania, Uganda and Rwanda.<br/>
+                        Lamlav Leisure Safaris is globally renowned for operating some of the most luxurious and distinct Safaris in Kenya, Tanzania, Uganda and Rwanda.<br/>
                         <br/>
                         We take pride in taking our esteemed guests to some of the most elegant and experimental camps and lodges, where each lodge or camp impeccably captures the true essence of the wild and its natural surroundings.                 
                     </p>
