9f2bbec6db1a4432c33e8f82fb9da7b3aff9e71b
Je4stine <je4stine@gmail.com>
2023-04-13 13:22:10 +0300

    Build 1000

--- src/Components/Destination/Destination.jsx
+++ src/Components/Destination/Destination.jsx
@@ -20,9 +20,10 @@ const Destination =()=>{
                 <img src={Destination1}/>
                 <h1 className=' font-[SourceSerifPro-Black] text-2xl my-5 text-red-900'> LAMLAV  MARA SERENGETI DELUXE</h1>
                 <p className=' font-[SourceSerifPro-Regular] lg:px-[100px]'>
-                    Experience  a truly  memorable and unabashedly  indulgent  Safari  in the Mara and Serengeti  ecosystems.From an amazing  wildlife  excursions  in the city, to the two slum-dunk  destinations at the heart of the world’s  most important  animal migration corridors,the iconic Maasai Mara and Serengeti  game reserves,experience  the most immersive  and authentic  adventure with three scheduled  flights  to expedite  your  travel  between the camps.
+                Experience  a truly  memorable and  unapologetically  indulging Safari  in the Mara and Serengeti  ecosystems.
+                Begining with an amazing  wildlife  excursions  in Nairobi city, then straight to  two slum-dunk  destinations at the heart of the world’s  most important  animal migration corridors, experience  the most immersive  and authentic  adventure, with three scheduled  flights  to expedite  your  travel  between the camps.
                 </p>
-                <p className=' my-5 text-lg font-[SourceSerifPro-Regular] text-red-900'> 9days/8nights from $ 9,700 per person.</p>
+                <p className=' my-5 text-lg font-[SourceSerifPro-Regular] text-red-900'> 10days/9 nights from $ 9,700 per person.</p>
                 <a href='/mara' className=' text-red-900 border border-red-900 rounded-md p-3 hover:bg-red-900 hover:text-white duration-500 left-16'>
                     <h1 className=' font-[SourceSerifPro-Black]'>Learn More</h1>
                 </a>
@@ -32,9 +33,11 @@ const Destination =()=>{
                 <img src={Destination3}/>
                 <h1 className=' font-[SourceSerifPro-Black] text-2xl my-5 text-red-900'> GREAT MIGRATION  SAFARI - Kenyan  Edition </h1>
                 <p className=' font-[SourceSerifPro-Regular] lg:px-[100px]'>
-                    An unabashedly  luxurious  safari, favoured  by many who  desire to to explore Africa's  legendary  savannahs,  and witness -first hand - the  breath-taking  wildebeest  migrations. This classic  Safari  covers three of Kenya’s great wildlife havens: The Nairobi National  Park, The Lewa Conservancy, in the dramatic  Laikipia Plateau,  and the iconic Maasai Mara game reserve, which is the centerpiece  of wildlife spectacles. Three exhilarating  flights between  the luxury camps and lodges  (and  back to the city) are meant  to expedite  your  travel.
+                Discover the wonders of Kenya with a touch of luxury.
+                This is a high-end Kenyan safari that's very popular among those who want to experience Africa's renowned savannahs and witness the awe-inspiring wildebeest migrations up close. The safari covers three of Kenya's top wildlife sanctuaries: Nairobi National Park, Lewa Conservancy in the Laikipia Plateau, and the famous Maasai Mara game reserve, which is known for its incredible wildlife sightings. To make travel faster and more convenient, you'll take three thrilling flights between the luxurious camps and lodges, as well as back to the city.
+                During your safari adventure, you'll enjoy a comfortable and leisurely stay in top-notch accommodations, such as the Fairmont Norfolk Hotel in the heart of the city, the Elewana Lewa Safari Camp in Laikipia, and the beautifully luxurious Mara Serena Lodge in Maasai Mara.
                 </p>
-                <p className=' my-5 text-lg font-[SourceSerifPro-Regular] text-red-900'> 9 days/8nights, from $9000 per person.</p>
+                <p className=' my-5 text-lg font-[SourceSerifPro-Regular] text-red-900'> 10days/9nights, from $9,127 per person.</p>
                 <a href='/migration' className=' text-red-900 border border-red-900 rounded-md p-3 hover:bg-red-900 hover:text-white duration-500 left-16'>
                     <h1 className=' font-[SourceSerifPro-Black]'>Learn More</h1>
                 </a>
@@ -44,7 +47,8 @@ const Destination =()=>{
                 <img src={Destination2}/>
                 <h1 className=' font-[SourceSerifPro-Black] text-2xl my-5 text-red-900'> LAMLAV  HONEYMOON SAFARI </h1>
                 <p className=' font-[SourceSerifPro-Regular] lg:px-[100px]'>
-                    Lamlav Honeymoon Safari offers the newly weds  a chance to celebrate their Mariage In style at some of the best honeymoon Spots In Kenya, with a promise  of luxurious respites at the crème de la crème accommodations: the Hemingways hotel In Nairobi, Kicheche Safari Camp In Ol Pejeta Conservancy, South of Laikipia, Ol Seki Hemingways in Maasai Mara; and at the Hemingway’s hotel In Watamu. Explore  and make memories  that will last a lifetime. 
+                An adventure  that feeds your Soul.
+                By commemorating your union in a grand fashion, at some of the finest honeymoon destinations in Kenya, Lamlav Honeymoon  Safari presents you an opportunity  to discover  and create  unforgettable  memories that you will forever  cherish. This safari package guarantees lavish stays at top-rated accommodations that includes: the Hemingways Hotel in Nairobi, Kicheche Safari Camp in the southern part of Laikipia's Ol Pejeta Conservancy, Ol Seki Hemingways in Maasai Mara, and the renowned Hemingway's Hotel in the beguiling Watamu. 
                 </p>
                 <p className=' my-5 text-lg font-[SourceSerifPro-Regular] text-red-900'> 12days/11Nights from  from $ 13,055 per person.</p>
                 <a href='/honeymoon' className=' text-red-900 border border-red-900 rounded-md p-3 hover:bg-red-900 hover:text-white duration-500 left-16'>
@@ -54,11 +58,12 @@ const Destination =()=>{
 
             <div className=" w-full mb-20 relative bg-white lg:w-[90%] m-auto p-5 flex flex-col justify-center items-center rounded-md shadow-md">
                 <img src={Destination4}/>
-                <h1 className=' font-[SourceSerifPro-Black] text-2xl my-5 text-red-900'> Amboseli</h1>
+                <h1 className=' font-[SourceSerifPro-Black] text-2xl my-5 text-red-900'> LAMLAV BUSH  AND BEACH SAFARI </h1>
                 <p className=' font-[SourceSerifPro-Regular] lg:px-[100px]'>
-                    Amboseli National Park is located in southern Kenya, near the border with Tanzania. It is known for its large herds of elephants and its spectacular views of Mount Kilimanjaro, the highest mountain in Africa, which can be seen in the background.
+                MARA -TSAVO –WATAMU. Explore the Kenyan  wildlife  havens  in Style.
+                Bush and beach is one of Lamlav's  most  comprehensive  safari  package  that  matchlessly introduces you to the country's most  treasured  and exhilarating  savannahs:  the city's Nairobi  National  park,  the Iconic Maasai  Mara Game  Reserve and Tsavo West and East National  Parks. The adventure  is then crowned with five days relaxations and explorations  of the pristine Kenyan coastline and beaches, while enjoying luxurious  stays at the magnificent Medina  Palms  Resort,  in the  bewitching Watamu.
                 </p>
-                <p className=' my-5 text-lg font-[SourceSerifPro-Regular] text-red-900'> 11 days/10 nights tour from $13,241 per person.</p>
+                <p className=' my-5 text-lg font-[SourceSerifPro-Regular] text-red-900'> 14Days/13Nights tour from $17,400 per person.</p>
                 <a href='/amboseli' className=' text-red-900 border border-red-900 rounded-md p-3 hover:bg-red-900 hover:text-white duration-500 left-16'>
                     <h1 className=' font-[SourceSerifPro-Black]'>Learn More</h1>
                 </a>
